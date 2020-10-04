9d293a8465d6259967350aaaa8c13e04a9446416
green <ilovesocal201213@yahoo.com>
2020-10-04 08:59:52 +0000

    revision

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 2020_10_04_045652) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
-    t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
