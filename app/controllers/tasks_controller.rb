03783d11679afb1ce4957b589aa7bf6c76a556e5
green <ilovesocal201213@yahoo.com>
2020-10-24 10:04:56 +0000

    HTMLコード修正、不要要素削除

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,8 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  # before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
-  
-  
+   
   def index
       @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(5)
   end
@@ -50,10 +49,7 @@ class TasksController < ApplicationController
 
 
   private
-  def set_task
-    @task = Task.find(params[:id])
-  end
- 
+
   def correct_user
      @task = current_user.tasks.find_by(id: params[:id])
      unless @task
